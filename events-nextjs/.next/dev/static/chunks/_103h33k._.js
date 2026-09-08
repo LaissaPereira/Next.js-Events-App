27832051e217eb62a57092e0e6d604ddfd5c8547
LaissaPereira <laissa.pereira25@gmail.com>
2026-09-08 10:32:28 +0200

    Remove stray nested .next build cache; broaden gitignore

--- events-nextjs/.next/dev/static/chunks/_103h33k._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_next_dist_0mrnf9s._.js","static/chunks/src_app_globals_162hn9o.css","static/chunks/node_modules_react-hook-form_dist_index_esm_mjs_0b0kr13._.js","static/chunks/node_modules_zod_v4_classic_0qgufeu._.js","static/chunks/node_modules_zod_v4_core_15b2lfp._.js","static/chunks/node_modules_zod_v4_locales_0zbpmps._.js","static/chunks/node_modules_1wvk2rr._.js","static/chunks/src_0e1gd69._.js"],
-    source: "entry"
-});
